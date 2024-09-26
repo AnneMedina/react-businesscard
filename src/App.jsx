c345ed4249f86a9663b4396bac6475816d7d055a
Anne Medina <carmelaanne.medina@gmail.com>
2024-09-26 14:23:44 +0930

    Created skeleton html and added some css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <div className="name"></div>
+      <div className="profession"></div>
+      <div className="website"></div>
+
+      <div className="cta">
+        <button className="cta-button">Email</button>
+        <button className="cta-button">LinkedIn</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+      <div className="about">
+        <h3>About</h3>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          I am a full-stack developer proficient in JavaScript, CSS, HTML, and
+          React for front-end development, as well as Node.js and PHP for
+          back-end solutions. I am committed to adhering to coding standards,
+          guidelines, and best practices, ensuring high-quality and maintainable
+          code. I prioritize forward-thinking design and scalability in my work
+          to mitigate potential technical debt and support long-term project
+          success.
+        </p>
+
+        <h3>Interests</h3>
+        <p>
+          I enjoy exploring and analyzing ideas that can benefit others. In my
+          downtime, I find joy in sophisticated coloring books, where I
+          experiment with blending colored pencils. I also play badminton and
+          love swimming during the summer months.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
