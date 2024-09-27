61283acabde40300c64f42f8379914a5c7181109
Anne Medina <carmelaanne.medina@gmail.com>
2024-09-27 15:38:20 +0930

    Added some footer icons and border radius around the card

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,57 @@ import "./App.css";
 import mail from "./assets/mail.svg";
 import linkedin from "./assets/linkedin.svg";
 import picture from "./assets/appleprofilepic.jpeg";
+import github from "./assets/github.svg";
+import facebook from "./assets/facebook.svg";
 
 function App() {
   return (
     <div className="main-container">
       <div className="card">
         <img src={picture} alt="card-image" className="card-image" />
-        <div className="name">Anne Medina</div>
-        <div className="profession">Web Application Developer</div>
-        <a href="https://www.gmanetwork.com/news" className="website">
-          annemedina.website
-        </a>
-
-        <div className="connect">
-          <a
-            href="mailto:anne.medina.au@gmail.com"
-            className="connect-links mail"
-          >
-            <img className="connect-logos" src={mail} />
-            Email
+        <div className="card-text">
+          <div className="name">Anne Medina</div>
+          <div className="profession">Web Application Developer</div>
+          <a href="https://www.gmanetwork.com/news" className="website">
+            annemedina.website
           </a>
-          <a
-            href="https://www.linkedin.com/in/carmelaanne-medina/"
-            className="connect-links linkedin"
-            target="_blank"
-          >
-            <img className="connect-logos" src={linkedin} />
-            LinkedIn
-          </a>
-        </div>
 
-        <div className="about">
-          <h3>About</h3>
-          <p>
-            I am a full-stack developer proficient in JavaScript, CSS, HTML, and
-            React for front-end development, as well as Node.js and PHP for
-            back-end solutions. I am committed to adhering to coding standards,
-            guidelines, and best practices, ensuring high-quality and
-            maintainable code.
-          </p>
+          <div className="connect">
+            <a
+              href="mailto:anne.medina.au@gmail.com"
+              className="connect-links mail"
+            >
+              <img className="connect-logos" src={mail} />
+              Email
+            </a>
+            <a
+              href="https://www.linkedin.com/in/carmelaanne-medina/"
+              className="connect-links linkedin"
+              target="_blank"
+            >
+              <img className="connect-logos" src={linkedin} />
+              LinkedIn
+            </a>
+          </div>
 
-          <h3>Interests</h3>
-          <p>
-            I enjoy exploring and analyzing ideas that can benefit others. In my
-            downtime, I find joy in sophisticated coloring books, where I
-            experiment with blending colored pencils. I also play badminton and
-            love swimming during the summer months.
-          </p>
+          <div className="about">
+            <h3>About</h3>
+            <p>
+              I am a full-stack developer proficient in JavaScript, CSS, HTML,
+              and React for front-end development, as well as Node.js and PHP
+              for back-end solutions. I am committed to adhering to coding
+              standards, guidelines, and best practices, ensuring high-quality
+              and maintainable code.
+            </p>
+
+            <h3>Interests</h3>
+            <p>
+              I enjoy exploring and analyzing ideas that can benefit others. In
+              my downtime, I find joy in sophisticated coloring books, where I
+              experiment with blending colored pencils. I also play badminton
+              and love swimming during the summer months.
+            </p>
+          </div>
         </div>
       </div>
     </div>
